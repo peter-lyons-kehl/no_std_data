01bd5bc0f41315bcc7143abc355cd7c9eadafee6
Peter Kehl <peter.kehl@gmail.com>
2022-08-28 22:12:39 -0700

    Leakage test harness. Reorg 03 <-> 04.

--- 00_utils/src/api_tests_mut/wipe_on_mut.rs
+++ 00_utils/src/api_tests_mut/wipe_on_mut.rs
@@ -2,7 +2,7 @@
 
 extern crate alloc;
 
-use crate::api_tests_mut::{RnaTraitMut, RnaTraitMutLeakStorage, WithStorageLeaked};
+use crate::api_tests_mut::{self, RnaTraitMut, RnaTraitMutLeakStorage, WithStorageLeaked};
 use crate::{DnaTrait, OurResult, RnaTrait};
 use alloc::vec::Vec;
 
@@ -16,6 +16,10 @@ pub trait Tests {
     fn test_modify_string_based_rna_mutation_does_not_leak<'a>(
         with_storage_leaked: WithStorageLeaked<'a, Self::Rna<'a>, bool>,
     ) {
-        //@TODO
+        let rna = api_tests_mut::cga_modified_to_u::<Self::Rna<'_>>();
+
+        let leaks = api_tests_mut::leaks_g_or_a(&rna, with_storage_leaked);
+
+        assert!(!leaks);
     }
 }
