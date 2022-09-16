9a07288eb844ad6ed436f1dab40b353661cb8caa
Peter Kehl <peter.kehl@gmail.com>
2022-09-15 17:40:20 -0700

    Experimenting with CodeTour

--- 00_utils/src/lib.rs
+++ 00_utils/src/lib.rs
@@ -9,8 +9,7 @@ where
 {
     fn new(dna: &'a str) -> OurResult<Self>;
     /// The common practice is that `into_*` methods consume `self` (rather than taking it by
-    /// reference). However, it's OK to take by reference where possible - making the API more
-    /// flexible.
+    /// reference). However, it's OK to take by reference where possible - making the API flexible.
     #[allow(clippy::wrong_self_convention)]
     fn into_rna(&self) -> Rna;
 }
